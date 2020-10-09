3c5a8d8c3203ef6af4aa389252058df4fa6406c9
TeoSparkles <catapusantp@byui.edu>
2020-10-09 14:29:53 +0800

    Okay now.

--- src/suchjose/Main.java
+++ src/suchjose/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello group, my name is Jose");
+        System.out.println("Hello Jose, my name is Teo.");
     }
 }
