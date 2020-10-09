41ddc066c7d53910a12e503defcc77c50e90b1a6
suchjose84 <65042405+suchjose84@users.noreply.github.com>
2020-10-09 11:45:26 +0800

    added text "group"

--- src/suchjose/Main.java
+++ src/suchjose/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello, my name is Jose");
+        System.out.println("Hello group, my name is Jose");
     }
 }
